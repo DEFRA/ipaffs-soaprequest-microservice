0d0025524913d91fe907a995d15ae03a19d1e259
kaizarl <kaizarl@kainos.com>
2019-04-17 17:01:03 +0100

    IMTA-4820 implement health check

---
+++ integration/src/test/java/uk/gov/defra/tracesx/soaprequest/integration/TestRoot.java
@@ -0,0 +1,23 @@
+package uk.gov.defra.tracesx.soaprequest.integration;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.defra.tracesx.soaprequest.integration.properties.Properties.SERVICE_BASE_URL;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+public class TestRoot {
+
+    private static final String ROOT_REQUEST_ENDPOINT = "%s/";
+
+    @Test
+    public void testRootWithNoAuthentication() {
+        String url = String.format(ROOT_REQUEST_ENDPOINT, SERVICE_BASE_URL);
+
+        Response response = given().header("Accept", "application/json").when().get(url);
+
+        response.then().statusCode(200);
+        assertThat(response.body().asString()).isEmpty();
+    }
+}
