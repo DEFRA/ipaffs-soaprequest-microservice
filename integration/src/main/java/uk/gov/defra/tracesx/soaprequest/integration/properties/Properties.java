c26acb5a13162e65ead3548f28ae9651a90ba93c
Piotr Dudkiewicz <piotr.dudkiewicz@defra.onmicrosoft.com>
2019-05-17 13:51:14 +0200

    IMTA-5018: microservice can be run locally using .env file

--- integration/src/main/java/uk/gov/defra/tracesx/soaprequest/integration/properties/Properties.java
+++ integration/src/main/java/uk/gov/defra/tracesx/soaprequest/integration/properties/Properties.java
@@ -1,7 +1,5 @@
 package uk.gov.defra.tracesx.soaprequest.integration.properties;
 
-import org.apache.commons.lang3.StringUtils;
-
 /**
  * This is properties class.
  */
@@ -13,47 +11,6 @@ public final class Properties {
   private Properties() {
   }
 
-  /**
-   * TEST_OPENID_TOKEN_SERVICE_URL.
-   * @param property
-   * @param envKey
-   */
-  public static final String TEST_OPENID_TOKEN_SERVICE_URL = getPropertyOrEnv(
-    "test.openid.service.url", "TEST_OPENID_TOKEN_SERVICE_URL");
-  /**
-   * TEST_OPENID_TOKEN_SERVICE_AUTH_USERNAME.
-   * @param property
-   * @param envKey
-   */
-  public static final String TEST_OPENID_TOKEN_SERVICE_AUTH_USERNAME =
-          getPropertyOrEnv(
-    "test.openid.service.auth.username",
-                  "TEST_OPENID_TOKEN_SERVICE_AUTH_USERNAME");
-  /**
-   * TEST_OPENID_TOKEN_SERVICE_AUTH_PASSWORD.
-   * @param property
-   * @param envKey
-   */
-  public static final String TEST_OPENID_TOKEN_SERVICE_AUTH_PASSWORD =
-          getPropertyOrEnv(
-    "test.openid.service.auth.password",
-                  "TEST_OPENID_TOKEN_SERVICE_AUTH_PASSWORD");
   public static final String SERVICE_BASE_URL = System
-    .getProperty("service.base.url", "http://localhost:4000");
-
-  /**
-   * getPropertyOrEnv.
-   * @param property propertyName.
-   * @param envKey envKeyName.
-   * @return Get the property.
-   */
-  private static String getPropertyOrEnv(final String property,
-                                         final String envKey) {
-    String value = System.getProperty(property);
-    if (StringUtils.isEmpty(value)) {
-      value = System.getenv(envKey);
-    }
-    return value;
-  }
-
+    .getProperty("service.base.url", "http://localhost:5260");
 }
--- integration/src/test/java/uk/gov/defra/tracesx/soaprequest/integration/TestAdminAuthentication.java
+++
@@ -1,5 +0,0 @@
-package uk.gov.defra.tracesx.soaprequest.integration;
-
-import uk.gov.defra.tracesx.common.security.tests.AbstractAdminAuthenticationTest;
-
-public class TestAdminAuthentication extends AbstractAdminAuthenticationTest {}
