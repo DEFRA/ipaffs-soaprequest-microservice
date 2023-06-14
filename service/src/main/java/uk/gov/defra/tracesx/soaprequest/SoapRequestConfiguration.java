1e505ef766dcbc79b240a9d899146adc74f2f77a
jenkins <jenkins@APSIMPJENSRV001>
2023-06-14 11:04:33 +0000

    13.1.0 release code

--- service/src/main/java/uk/gov/defra/tracesx/soaprequest/SoapRequestConfiguration.java
+++ service/src/main/java/uk/gov/defra/tracesx/soaprequest/SoapRequestConfiguration.java
@@ -1,6 +1,10 @@
 package uk.gov.defra.tracesx.soaprequest;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,5 +13,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableConfigurationProperties
 @EnableJpaRepositories("uk.gov.defra.tracesx.soaprequest.dao.repositories")
 public class SoapRequestConfiguration {
+  @Value("${spring.datasource.url}")
+  private String jdbcUrl;
 
+  @Value("${spring.datasource.managed-identity-url}")
+  private String managedIdentityJdbcUrl;
+
+  @Value("${features.enable-managed-identity-auth}")
+  private boolean enableManagedIdentityAuth;
+
+  @Bean
+  public DataSource getDataSource() {
+    if (enableManagedIdentityAuth) {
+      return DataSourceBuilder.create().url(managedIdentityJdbcUrl).build();
+    }
+    return DataSourceBuilder.create().url(jdbcUrl).build();
+  }
 }
