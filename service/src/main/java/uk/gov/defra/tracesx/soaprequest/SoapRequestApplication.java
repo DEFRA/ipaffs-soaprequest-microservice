1aabffcecbae22189c7f0ac0ff65f736772f788e
jenkins <jenkins@APSIMPJENSRV001>
2023-10-12 11:22:38 +0000

    14.0.0 release code

--- service/src/main/java/uk/gov/defra/tracesx/soaprequest/SoapRequestApplication.java
+++ service/src/main/java/uk/gov/defra/tracesx/soaprequest/SoapRequestApplication.java
@@ -2,9 +2,10 @@ package uk.gov.defra.tracesx.soaprequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @ComponentScan("uk.gov.defra.tracesx")
 public class SoapRequestApplication {
   public static void main(String[] args) {
